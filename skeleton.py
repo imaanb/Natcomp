8d1d3ea5fb3cda1bcc8aae9a74b3475733feb83e
Rick Wierenga <rick_wierenga@icloud.com>
2023-10-09 22:37:53 +0200

    automaton

--- skeleton.py
+++ skeleton.py
@@ -7,29 +7,49 @@ class CellularAutomata:
     """Skeleton CA, you should implement this."""
 
     def __init__(self, rule_number: int):
-        """Intialize the cellular automaton with a given rule number"""
-        self.rule_number = rule_number 
-        
+        """ Intialize the cellular automaton with a given rule number """
+        self.rule_number = rule_number
 
-    def generate_rule_table(self): #added by Imaan 
-        "Convert rule number into binary rule table of lenght 8"
-        table = '{0:08b}'.format(self.rule_number)
-        return(table)
+    def generate_rule_table(self, rule):
+        # 1 = living cell, 0 = dead cell
+        rule_binary = [int(x) for x in bin(rule)[2:].zfill(8)[::-1]]
+        rule_table = {}
 
-    
+        for state in range(8):
+            state_binary = format(state, '03b')
+            state_tuple = tuple(int(bit) for bit in state_binary)
+            rule_table[state_tuple] = rule_binary[state]
+
+        return rule_table
+
+    def apply_rule(self, rule, state):
+        rule_table = self.generate_rule_table(rule)
+
+        # Extend the state by wrapping around
+        extended_state = [0] + state + [0]
+
+        # Apply the rule to generate the next state
+        new_state = []
+
+        for i in range(1, len(extended_state) - 1):
+            # Calculate the neighborhood configuration
+            neighborhood = (extended_state[i - 1], extended_state[i], extended_state[i + 1])
+
+            # Get the corresponding bit from the rule table and convert it to an integer
+            new_cell_value = rule_table[neighborhood]
+
+            # Append the result to the new state
+            new_state.append(new_cell_value)
+
+        return new_state
 
     def __call__(self, c0: typing.List[int], t: int) -> typing.List[int]:
         """Evaluate for T timesteps. Return Ct for a given C0."""
-        hist = [c0.copy()]
-        print()
-
-        for i in range(t): 
-            state_curr = history[-1]  #how many times are we regenating a binary string? 
-            next_state = np.zeros(len(state_curr)) #intializing next string 
-            for j in range(len(state_curr)):       #defining the neighbourhood ---> wat about edge cases? 
-                neighbours = [state_curr[j-1], state_curr[j], state_curr[j+1]]
-
-
+        state = c0
+        state = list(state)
+        for _ in range(t):
+            state = self.apply_rule(self.rule_number, state)
+        return state
 
     @classmethod
     def test(cls, rule_number, c0, ct, t=1):
@@ -45,10 +65,11 @@ if __name__ == "__main__":
     CellularAutomata.test(0, x0, np.zeros(7, dtype=int))
     CellularAutomata.test(30, x0, [0,1,1,0,0,1,0], 2)
 
-
-
-instance1 = CellularAutomata(30)
-
-print(CellularAutomata.generate_rule_table(instance1))
-
-
+    import sys
+    rule_number = int(sys.argv[1])
+    ca = CellularAutomata(rule_number)
+    x0 = np.zeros(60, dtype=int)
+    x0[x0.size // 2] = 1
+    for t in range(20):
+        x0 = ca(x0, 1)
+        print(''.join(['*' if cell == 1 else ' ' for cell in x0]))
