0194b8b208f748bec420960183ccf969b154a6d4
Rick Wierenga <rick_wierenga@icloud.com>
2023-10-13 12:38:50 +0200

    figure for multiple rules

--- experiment.py
+++ experiment.py
@@ -4,10 +4,9 @@ import matplotlib.pyplot as plt
 from skeleton import CellularAutomata
 
 
-N_REPEATS = 10
+N_REPEATS = 100
 N_STEPS = 100
 
-rule_number = int(sys.argv[1])
 
 def generate_runs(rule_number, n_steps):
     ca = CellularAutomata(rule_number)
@@ -22,14 +21,15 @@ def generate_runs(rule_number, n_steps):
 def count_nonzero(states):
     return [np.count_nonzero(state) / np.count_nonzero(states[0]) for state in states]
 
+
 # the longest string of equal symbols
 def longest_consecutive_string(states):
     result = []
-    
+
     for state in states:
         max_consecutive = 0
         current_consecutive = 0
-        
+
         for last_digit, digit in zip(state, state[1:]):
             if last_digit == digit:
                 current_consecutive += 1
@@ -38,7 +38,7 @@ def longest_consecutive_string(states):
                 current_consecutive = 0
 
         result.append(max_consecutive)
-    
+
     return result
 
 
@@ -65,39 +65,50 @@ def longest_surviving_cell_age(states):
     return results
 
 
-if __name__ == "__main__":
+def save_fig(datas, experiment_name):
+    plt.figure()
+    for rule_number, data in datas.items():
+        std = np.std(data, axis=0)
+        mean = np.mean(data, axis=0)
+        plt.plot(mean, label=rule_number)
+    # plt.fill_between(range(N_STEPS), mean - std, mean + std, alpha=0.2)
+    plt.legend()
+    plt.title(experiment_name)
+
+    rules_string = "-".join([str(rule_number) for rule_number in datas.keys()])
+    figname = f"{experiment_name}-{rules_string}.png"
+    plt.savefig(figname)
+
+
+def main():
     # python experiment.py 26
 
-    num_living = []
-    lcs = []
-    nc = []
-    a = []
-    for i in range(N_REPEATS):
-        states = generate_runs(rule_number, N_STEPS)
-        num_living.append(count_nonzero(states))
-        lcs.append(longest_consecutive_string(states))
-        nc.append(num_changed(states))
-        a.append(longest_surviving_cell_age(states))
+    rule_numbers = [int(x) for x in sys.argv[1:]]
+    if len(rule_numbers) == 0:
+        raise ValueError("Please provide at least one rule number")
 
-        for state in states:
-            print(''.join(['*' if cell == 1 else ' ' for cell in state]))
+    num_living = {rule_number: [] for rule_number in rule_numbers}
+    lcs = {rule_number: [] for rule_number in rule_numbers}
+    nc = {rule_number: [] for rule_number in rule_numbers}
+    a = {rule_number: [] for rule_number in rule_numbers}
+    for rule_number in rule_numbers:
+        for i in range(N_REPEATS):
+            np.random.seed(i)
+            states = generate_runs(rule_number, N_STEPS)
 
-    plt.clf()
-    plt.plot(np.mean(num_living, axis=0))
-    plt.fill_between(range(N_STEPS), np.mean(num_living, axis=0) - np.std(num_living, axis=0), np.mean(num_living, axis=0) + np.std(num_living, axis=0), alpha=0.2)
-    plt.savefig(f'num_living-{rule_number}.png')
+            num_living[rule_number].append(count_nonzero(states))
+            lcs[rule_number].append(longest_consecutive_string(states))
+            nc[rule_number].append(num_changed(states))
+            a[rule_number].append(longest_surviving_cell_age(states))
 
-    plt.clf()
-    plt.plot(np.mean(lcs, axis=0))
-    plt.fill_between(range(N_STEPS), np.mean(lcs, axis=0) - np.std(lcs, axis=0), np.mean(lcs, axis=0) + np.std(lcs, axis=0), alpha=0.2)
-    plt.savefig(f'longest_continuous_string-{rule_number}.png')
+            # for state in states:
+            #     print(''.join(['*' if cell == 1 else ' ' for cell in state]))
 
-    plt.clf()
-    plt.plot(np.mean(nc, axis=0))
-    plt.fill_between(range(N_STEPS), np.mean(nc, axis=0) - np.std(nc, axis=0), np.mean(nc, axis=0) + np.std(nc, axis=0), alpha=0.2)
-    plt.savefig(f'num_changed-{rule_number}.png')
+    save_fig(num_living, experiment_name='num_living')
+    save_fig(lcs, experiment_name='longest_continuous_string')
+    save_fig(nc, experiment_name='num_changed')
+    save_fig(a, experiment_name='age')
 
-    plt.clf()
-    plt.plot(np.mean(a, axis=0))
-    plt.fill_between(range(N_STEPS), np.mean(a, axis=0) - np.std(a, axis=0), np.mean(a, axis=0) + np.std(a, axis=0), alpha=0.2)
-    plt.savefig(f'age-{rule_number}.png')
+
+if __name__ == "__main__":
+    main()
